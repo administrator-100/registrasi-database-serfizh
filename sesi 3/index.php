3e16ba03158be13964f2b329b81ee8646a8c74c7
Kings-So <maumanjat@gmail.com>
2024-11-07 00:48:06 +0700

    Add files via upload

---
+++ sesi 3/index.php
@@ -0,0 +1,13 @@
+<?php
+  $nama = "Dunia";
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Halaman PHP</title>
+  </head>
+  <body>
+    <h1>Halo, <?php echo $nama; ?>!</h1>
+    <p>Ini adalah halaman PHP yang menampilkan konten dinamis.</p>
+  </body>
+</html>
